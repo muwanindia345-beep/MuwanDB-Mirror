d30ca0e5d148b17de334f939d08553a4d23afe14
muwanindia345-beep <muwanindia345@gmail.com>
2026-06-22 15:50:50 +0530

    Fixed

--- src/api/muwandb.js
+++ src/api/muwandb.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://muwandb-server-production.up.railway.app'\;
+const BASE_URL = 'https://muwandb-server-production.up.railway.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
